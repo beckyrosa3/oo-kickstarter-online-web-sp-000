06233cae24647308f1f09544913b69c6552d0058
Becky Rosa <becky_m_rosa@yahoo.com>
2021-05-15 23:40:32 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,18 +7,22 @@ class Backer
     @backed_projects = []
   end
 
-  def project=(project)
-    @project = project
-    project.back_project(self) unless project.backer.include?(self)
+  def back_project(project)
+    @backed_projects << project
+    #project.author = self unless project.backer == self
   end
 
   def backed_projects
     @backed_projects
   end
 
-  def back_project(project)
-    @backed_projects << project
-    #project.author = self unless project.backer == self
+  def project=(project)
+    @project = project
+    project.back_project(self) unless project.backer.include?(self)
   end
+
+
+
+
 
 end
